85927ad2735b0f53727617a7af59eeda128e8129
Tom Nguyen <dungnguyen_2025@depauw.edu>
2023-06-27 00:23:40 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+
+const apiKey = "RL0iYC4bnEo2bhHVr3Io7Q==pustejX98lfGGeTc";
+
+const options = {
+    method: "GET",
+    headers: {
+      "X-Api-Key": apiKey,
+    },
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+async function getJoke() {
+    try {
+        jokeEl.innerText = "Updating...";
+        btnEl.disabled = true;
+        btnEl.innerText = "Loading...";
+        const response = await fetch(apiURL, options);
+        const data = await response.json();
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a joke";
+        jokeEl.innerText = data[0].joke;
+    } catch (error) {
+        jokeEl.innerText = "An error happened, try again later";
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a joke";
+    }
+};
+
+btnEl.addEventListener("click", getJoke);
